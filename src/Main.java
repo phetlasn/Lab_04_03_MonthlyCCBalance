42b75a0b7d12c08394aaedb13b0a60625367f2b4
phetlasn <Souriya.n.phet@gmail.com>
2022-09-18 23:55:20 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        double balance = 5000;
+        double salesTax;
+        salesTax = balance * 0.17;
+
+        System.out.println("The interest applied after one month will be $ " + salesTax);
+    }
+}
